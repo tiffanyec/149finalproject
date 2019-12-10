9b00d6195b6065ed68a94888a43361ab32e1cc16
Tiffany <tiffanyec@berkeley.edu>
2019-12-10 11:58:38 -0800

    add crazyflie_clean and vrpn packages

---
+++ vrpn_client_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/eecs149_rst/vrpn_client_ws/src
+# Build directory: /home/eecs149_rst/vrpn_client_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(vrpn_client_ros)
---
+++ vrpn_client_ws/build/vrpn_client_ros/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/eecs149_rst/vrpn_client_ws/src/vrpn_client_ros/include;/opt/ros/kinetic/include".split(';') if "/home/eecs149_rst/vrpn_client_ws/src/vrpn_client_ros/include;/opt/ros/kinetic/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "geometry_msgs;tf2_ros".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lvrpn_client_ros;/opt/ros/kinetic/lib/libvrpn.a;/opt/ros/kinetic/lib/libquat.a;-lm;-l-lpthread".split(';') if "-lvrpn_client_ros;/opt/ros/kinetic/lib/libvrpn.a;/opt/ros/kinetic/lib/libquat.a;-lm;-l-lpthread" != "" else []
+PROJECT_NAME = "vrpn_client_ros"
+PROJECT_SPACE_DIR = "/home/eecs149_rst/vrpn_client_ws/devel"
+PROJECT_VERSION = "0.2.2"
