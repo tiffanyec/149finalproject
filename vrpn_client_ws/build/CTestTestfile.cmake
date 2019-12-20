b5d328afdef4517cd0d0858dba98cc21f08c02c8
Tiffany <tiffanyec@berkeley.edu>
2019-12-19 21:32:54 -0800

    file restructure

--- vrpn_client_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eecs149_rst/149finalproject/vrpn_client_ws/src
-# Build directory: /home/eecs149_rst/149finalproject/vrpn_client_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(vrpn_client_ros)
--- vrpn_client_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/eecs149_rst/149finalproject/vrpn_client_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/eecs149_rst/149finalproject/vrpn_client_ws/devel;/opt/ros/kinetic".split(';') if "/home/eecs149_rst/149finalproject/vrpn_client_ws/devel;/opt/ros/kinetic" != "" else []
